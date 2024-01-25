88a111ffcd3df5e271450e971d5a5bb70f24fb37
Dave Get <daveget2020@gmail.com>
2024-01-25 11:05:12 +0100

    modifiying nft assests

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,31 @@ import { useEffect, useMemo, useState } from "react";
 import Main from "./Main";
 import { RPC_ENDPOINT } from "../utils";
 
-import { ConnectionProvider } from "@solana/wallet-adapter-react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function Home() {
-  return <Main />;
+  const [mounted, setMounted] = useState(false);
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()]);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <ConnectionProvider
+      endpoint={RPC_ENDPOINT}
+      config={{ commitment: "confirmed" }}
+    >
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{mounted && <Main />}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,9 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const RPC_ENDPOINT = "https://metaplex.devnet.rpcpool.com/";
+export const RPC_ENDPOINT =
+  "https://prettiest-floral-mansion.solana-devnet.quiknode.pro/1e51909b0eb2d0c6e7b11aac0a714c8eab357bfd/";
 
 // Change this with your candy machine id
-export const CANDY_MACHINE_ID = new PublicKey("APDPkmr6dS2E4sCH6jVtFGX7dzuAbVnZkokikN4kyFiU");
+export const CANDY_MACHINE_ID = new PublicKey(
+  "APDPkmr6dS2E4sCH6jVtFGX7dzuAbVnZkokikN4kyFiU"
+);
